eaded9af41b4b10957e06e391bc071c246a4fb32
galyverasi <galyverasi@gmail.com>
2021-11-30 20:11:29 -0700

    ingredients and components

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import './App.css';
+import IngredientList from "./IngredientList"
+
+
+const ingredients = [
+  {name: 'Kaiser Bun', color: 'saddlebrown'},
+  {name: 'Sesame Bun', color: 'sandybrown'},
+  {name: 'Gluten Free Bun', color: 'peru'},
+  {name: 'Lettuce Wrap', color: 'olivedrab'},
+  {name: 'Beef Patty', color: '#3F250B'},
+  {name: 'Soy Patty', color: '#3F250B'},
+  {name: 'Black Bean Patty', color: '#3F250B'},
+  {name: 'Chicken Patty', color: 'burlywood'},
+  {name: 'Lettuce', color: 'lawngreen'},
+  {name: 'Tomato', color: 'tomato'},
+  {name: 'Bacon', color: 'maroon'},
+  {name: 'Onion', color: 'lightyellow'}
+]
+
+function App() {
+  return (
+    <div className="App">
+      <h1>burger stacker?</h1>
+      <div className="IngredientMenu">
+        <ul>
+          <li>List Items will go here :)</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
