258c3b8a3868ce0feb67f750135d3fe9a7fd8b71
galyverasi <galyverasi@gmail.com>
2021-12-05 11:43:54 -0700

    hooks on Ingredient

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -1,14 +1,16 @@
 // Ingredient is a child of IngredientList
 // gets the props of an individual ingredient, color and name
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Ingredient extends Component {
+const Ingredient = props => {
     
-    render () {
+    // render () {
         return (
-            <p style={{backgroundColor: this.props.ingredient.color}} onClick={this.props.clicky}>
-                { this.props.ingredient.name }
+            <p style={{backgroundColor: props.ingredient.color}} onClick={props.clicky}>
+                { props.ingredient.name }
             </p>
         )
     }
-}
+// }
+
+export default Ingredient
