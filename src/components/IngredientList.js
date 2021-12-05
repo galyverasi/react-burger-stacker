37f34ff7d0544863c346f90828bb19ca70a6fbd1
galyverasi <galyverasi@gmail.com>
2021-12-05 12:09:07 -0700

    hooks on IngredientList

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -1,14 +1,14 @@
 // ingredient list will loop over props(ingredients) 
 // produces an Ingredient component for every ingredient in the array
-import React, { Component } from 'react'
+import React from 'react'
 import Ingredient from './Ingredient'
 
-export default class IngredientList extends Component {
-    render () {
+const IngredientList = props => {
+    // render () {
         // here we need to store our group of ingredients(the component)
-        let allIngredients = this.props.ingredients.map(mapIng => (
-            <li onClick={this.props.add}>
-                <Ingredient ingredient={mapIng} />
+        let allIngredients = props.ingredients.map(mapIngredient => (
+            <li onClick={props.add}>
+                <Ingredient ingredient={mapIngredient} />
             </li>
         ))
         // then in our return, we'll render those as necessary
@@ -21,4 +21,6 @@ export default class IngredientList extends Component {
             </section>
         )
     }
-}
+//}
+
+export default IngredientList
